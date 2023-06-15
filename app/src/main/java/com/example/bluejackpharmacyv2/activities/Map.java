6d6a1900514e7b63cc62eecef845fa1e0b4191c8
raaizar <rahmanraiza@gmail.com>
2023-06-15 15:24:45 +0700

    fixing map bugs

--- app/src/main/java/com/example/bluejackpharmacyv2/activities/Map.java
+++ app/src/main/java/com/example/bluejackpharmacyv2/activities/Map.java
@@ -2,10 +2,8 @@ package com.example.bluejackpharmacyv2.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ImageButton;
 
 import com.example.bluejackpharmacyv2.R;
@@ -17,45 +15,41 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Map extends AppCompatActivity implements OnMapReadyCallback {
-
-    private GoogleMap map;
+    private GoogleMap gMap;
+    private double LAT = -6.20201, LONG = 106.78113;
+    private float ZOOM_LEVEL = 15.0f;
     private ImageButton backButton;
-    private final float ZOOM_LEVEL = 15.0f;
-    private final double LAT = -6.20201, LONG = 106.78113;
-    private final String MARKER_NAME = "Bluejack Pharmacy";
+    private String MARKER_NAME = "Bluejack Pharmacy";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
-
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(this);
-        } else {
-            Log.e("Showing map error!", "Map is null!");
-        }
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapFragment);
+        supportMapFragment.getMapAsync(this);
 
         initialize();
     }
 
-    private void initialize() {
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        gMap = googleMap;
+
+        LatLng latLng = new LatLng(LAT, LONG);
+        gMap.addMarker(new MarkerOptions().position(latLng).title(MARKER_NAME));
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, ZOOM_LEVEL));
+    }
+
+    public void initialize(){
         backButton = findViewById(R.id.back_button);
+
         setListener();
     }
 
-    private void setListener() {
-        backButton.setBackground(ContextCompat.getDrawable(this, R.drawable.clicked_blue));
-        backButton.setOnClickListener(e -> finish());
-    }
-
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        map = googleMap;
-
-        LatLng latlng = new LatLng(LAT, LONG);
-        map.addMarker(new MarkerOptions().position(latlng).title(MARKER_NAME));
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, ZOOM_LEVEL));
+    public void setListener(){
+        backButton.setOnClickListener(e -> {
+            finish();
+        });
     }
 }
